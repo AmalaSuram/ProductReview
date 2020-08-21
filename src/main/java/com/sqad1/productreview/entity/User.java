57716589ea246759154973fd68b679fbdcb8433a
Shraddha <bhureshr123@gmail.com>
2020-08-21 19:27:48 +0530

    Test cases are commited

--- src/main/java/com/sqad1/productreview/entity/User.java
+++ src/main/java/com/sqad1/productreview/entity/User.java
@@ -19,4 +19,36 @@ public class User {
 	private String email;
 	private String address;
 	private Long phoneNumber;
+	
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
